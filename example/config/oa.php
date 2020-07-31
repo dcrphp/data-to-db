c174437aef4c487f9a116fef96eb3cd38809732a
huanghuanjun <junqing124@126.com>
2020-07-31 17:25:56 +0800

    Update oa.php

--- example/config/oa.php
+++ example/config/oa.php
@@ -5,7 +5,7 @@ return array(
     'handler' => 'api', //类型  支持db和api获取数据
     'source_config' => array( //配置方式可看target.php
         'url' => '',  //api地址
-        'data_field' => '',  //数据列表字段 如data.list或data
+        'data_field' => '',  //数据列表是在哪个字段 比如返回是 array('ack'=>1,'data_list'=>array()) 则配置为data_list,多维请用.分隔，比如data.data_list
         'request_type' => 'get', //post or get
         'request_data' => array(), //请求的数据
     ),
